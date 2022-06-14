6d9fc50db2756e7202e2c2ec505f68fa7bdcaf81
Nick Story <storynickolasl@gmail.com>
2022-06-13 22:37:24 -0400

    Remove no longer needed functions

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-  document.getElementById('next').style.display='none' 
-  document.getElementById('previous').style.display='none' 
+  const previous = document.getElementById('previous')
+  const next = document.getElementById('next')
+
+  next.style.display='none' 
+  previous.style.display='none' 
 
   const states = ['Alabama','Alaska','Arizona','Arkansas','California','Colorado',
     'Connecticut','Delaware','District of Columbia','Florida','Georgia','Hawaii','Idaho',
@@ -40,7 +43,7 @@ document.addEventListener('DOMContentLoaded', () => {
     page=1
     e.preventDefault();
     document.getElementById('breweries-list').innerHTML = '';
-    breweries = []
+    // breweries = []
     city = document.getElementById('city').value.split(' ');
     city = city.join('_');
     state = document.getElementById('states').value
@@ -54,31 +57,21 @@ document.addEventListener('DOMContentLoaded', () => {
       .then((data) => breweryListMaker(data))
   }
 
-  //Fill breweries array from API data
-  // class brewery{
-  //   constructor(name, latitude, longitude, street, zip, url, phone){
-  //     this.name = name
-  //     this.latitude = latitude,
-  //     this.longitude = longitude,
-  //     this.street = street,
-  //     this.zip = zip,
-  //     this.url = url,
-  //     this.phone = phone
-  //   }
-  // }
-
-  // function createBrews(place) {
-  //   breweries.push(
-  //     new brewery(
-  //       place.name, 
-  //       place.latitude, 
-  //       place.longitude,
-  //       place.street,
-  //       place.postal_code,
-  //       place.website_url,
-  //       place.phone
-  //     ))
-  // }
+   //Add functionality to next/previous buttons if results have more than 20 breweries
+   document.getElementById('next').addEventListener('click', () => nextBtn())
+   document.getElementById('previous').addEventListener('click', () => previousBtn())
+ 
+   function nextBtn() {
+     page++
+     document.getElementById('breweries-list').innerHTML=''
+     getBreweries(city, state, page)
+   }
+ 
+   function previousBtn() {
+     page--
+     document.getElementById('breweries-list').innerHTML=''
+     getBreweries(city, state, page)
+   }
 
   //Click on brewery name for additional info
   function moreInfo(info) {
@@ -89,17 +82,22 @@ document.addEventListener('DOMContentLoaded', () => {
   function breweryListMaker(breweries) {
     let count = 1;
     let geo = []
-    breweries[0] == undefined ? document.getElementById('error').style.display='block' : document.getElementById('error').style.display='none'
-    page < 1 || breweries.length !== 20 ? document.getElementById('next').style.display='none' : document.getElementById('next').style.display='block'
-    page < 2 ? document.getElementById('previous').style.display='none' : document.getElementById('previous').style.display='block'
+    const error = document.getElementById('error').style
+
+    breweries[0] ? error.display = 'none' : error.display = 'block'
+    breweries.length < 20 ? next.style.display = 'none' : next.style.display = 'block'
+    page > 1 ? previous.style.display='block' : previous.style.display = 'none' 
+
     breweries.forEach((element) => {
       const newBrew = document.createElement('li')
       let address, website, phone
-      element.street === null ? address = '' : address = `${element.street}, ${element.city}`
-      element.website_url === null ? website = '' : website = `${element.website_url}`
-      element.phone === null ? phone = '' : phone = `(${element.phone.slice(0,3)})-${element.phone.slice(3,6)}-${element.phone.slice(6,10)}`
+      // element.street ? address = `${element.street}, ${element.city}` : address = ''
+      address = element.street ? `${element.street}, ${element.city}` : ''
+      website = element.website_url ? `${element.website_url}` : ''
+      phone = element.phone ? `(${element.phone.slice(0,3)})-${element.phone.slice(3,6)}-${element.phone.slice(6,10)}` : ''
+      
       newBrew.innerHTML = `
-        <p id=${count}>${element.name}</P>
+        <p id=${count}>${element.name}</p>
         <div class='breweries' id=m${count}>
           <h3>${element.name}</h3>
           <h4>${address}</h4>
@@ -107,7 +105,7 @@ document.addEventListener('DOMContentLoaded', () => {
           <a href=${website}>${website} </a>
         </div>
       `
-      if (typeof element.longitude === 'string') {
+      if (element.longitude) {
         geo.push({
           name: element.name, 
           latitude: element.latitude, 
@@ -143,21 +141,5 @@ document.addEventListener('DOMContentLoaded', () => {
     geo.forEach(element => {
       L.marker([element.latitude, element.longitude]).addTo(map).on('click', () => alert(element.name)).addTo(map)
     })
-  }
-
-  //Add functionality to next/previous buttons if results have more than 20 breweries
-  document.getElementById('next').addEventListener('click', () => next())
-  document.getElementById('previous').addEventListener('click', () => previous())
-
-  function next() {
-    page++
-    document.getElementById('breweries-list').innerHTML=''
-    getBreweries(city, state, page)
-  }
-
-  function previous() {
-    page--
-    document.getElementById('breweries-list').innerHTML=''
-    getBreweries(city, state, page)
   }
 })
